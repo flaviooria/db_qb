a7ce1028eebb8d4080a33928670f694691fc3cc5
Flavio Oria <flavio.oriap@gmail.com>
2024-05-29 13:15:13 +0200

    :sparkles: Add settings file to load enviroment variables

--- db_query_builder/settings.py
+++ db_query_builder/settings.py
@@ -1,12 +1,82 @@
-from pydantic import Field
+from enum import Enum
+
+from pydantic import Field, computed_field, field_validator, StringConstraints
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing_extensions import ClassVar, Annotated
+
+
+class DatabaseTypes(str, Enum):
+    MYSQL = 'mysql'
+    POSTGRES = 'postgres'
+    MARIADB = 'mariadb'
+
+
+MotorString = Annotated[
+    str, StringConstraints(strip_whitespace=True, to_lower=True,
+                           pattern=r'^[postgres|mysql|mariadb]')]
 
 
 class Settings(BaseSettings):
-    auth_key: str = Field(..., validation_alias='my_auth_key')
+    AUTH_KEY: str = Field(..., validation_alias='MY_AUTH_KEY')
+
+    # ENV VARIABLE FOR CONNECT TO DB
+    DB_LOCALHOST: str = Field(default='localhost')
+    DB_NAME: str
+    DB_USERNAME: str
+    DB_PASSWORD: str
+    DB_PORT: int = Field(default=5432)
+    DB_URI: str
+    DB_SCHEME: str = Field(..., description='Driver de motor de base de datos', examples=['psycopg2'])
+    DB_MOTOR: MotorString = Field(
+        ..., description='Motor de base de datos', examples=['postgres', 'myqsl', 'mariadb'])  # type ignore
+
+    # ClassVar se utiliza para indicar que es un atributo privado o mejor dicho de clase
+    _allowed_schemes: ClassVar[str] = ['psycopg2', 'psycopg', 'pg8000', 'asyncpg', 'psycopg2cffi']
+
+    _db_motors: ClassVar[str] = ['postgres', 'myqsl', 'mariadb']
+
+    @field_validator('DB_SCHEME')
+    @classmethod
+    def verify_scheme(cls, value: str):
+        if isinstance(value, str):
+            if value not in cls._allowed_schemes:
+                raise ValueError(f'Driver de base de datos no se encuentra en: {str(cls._allowed_schemes)}')
+            return value
+
+    @field_validator('DB_MOTOR')
+    @classmethod
+    def verify_db_motor(cls, value: str):
+        if isinstance(value, str):
+            if value not in cls._db_motors:
+                raise ValueError(f'Motor de base de datos no se encuentra en: {str(cls._db_motors)} ')
+            return value
+
+    @computed_field
+    @property
+    def uri(self) -> str:
+        _scheme = ""
+
+        if self.DB_URI is not None and self.DB_URI != "":
+            return self.DB_URI
+
+        if self.DB_MOTOR == DatabaseTypes.MYSQL:
+            _scheme = 'mysql+'
+        if self.DB_MOTOR == DatabaseTypes.POSTGRES:
+            _scheme = 'postgresql+'
+        if self.DB_MOTOR == DatabaseTypes.MARIADB:
+            _scheme = 'mariadb+'
+
+        _scheme += self.DB_SCHEME
+
+        return MultiHostUrl.build(scheme=_scheme, host=self.DB_LOCALHOST,
+                                  username=self.DB_USERNAME,
+                                  password=self.DB_PASSWORD, port=self.DB_PORT, path=self.DB_NAME).unicode_string()
 
     # Añadimos soporte de Dotenv -> https://docs.pydantic.dev/latest/concepts/pydantic_settings/#dotenv-env-support
     model_config = SettingsConfigDict(env_file='../.env', env_file_encoding='utf-8', extra='ignore')
+
+    # extra indica que nuestro ficher .env puede tener claves que no necesariamente esten declarada en esta clase
 
 
 config = Settings()
