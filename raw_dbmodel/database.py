051b704c4ab499111cd3c63e7da44ae27e0af07e
Flavio Oria <flavio.oriap@gmail.com>
2024-06-16 00:08:25 +0200

    ✨ create method to create tables if is subclass of SQLModel and is a table

--- raw_dbmodel/database.py
+++ raw_dbmodel/database.py
@@ -3,9 +3,11 @@ import importlib
 import os.path
 import pathlib
 import types
+from typing import List
 
 from sqlalchemy import Engine, inspect
 from sqlmodel import SQLModel, create_engine
+from typing_extensions import Type
 
 from raw_dbmodel.settings import config
 
@@ -96,7 +98,27 @@ def init_db(module: types.ModuleType):
         print('El error viene de aquí => ', ex)
 
 
-__all__ = ["init_db", "engine"]
+def creat_tables(classes: List[Type]):
+    tables: list[Type[SQLModel]] = []
+    for _cls in classes:
+        if isinstance(_cls, type):
+            try:
+                if issubclass(_cls, SQLModel) and _cls is not SQLModel and hasattr(_cls, '__table__'):
+                    tables.append(_cls)
+                else:
+                    print(f"{_cls.__name__} class is not a table")
+            except TypeError as te:
+                print(te)
+
+    if len(tables) > 0:
+        print_tables = [f"{cls.__name__} class has been added as table name: {cls.__tablename__}" for cls in tables]
+        print(*print_tables, sep='\n')
+        SQLModel.metadata.create_all(bind=engine, tables=[cls.__table__ for cls in tables if hasattr(cls, '__table__')])
+    else:
+        print('Could not create the tables, check that the imported classes are correct.')
+
+
+__all__ = ["init_db", "engine", "creat_tables"]
 
 
 def __dir__() -> list[str]:
